8496dff4ba3e1f00b4e76f90eabd09b0be3111db
nec02349 <stephenhuang804@gmail.com>
2017-09-09 17:27:39 -0400

    Finished sentence generating
    Finished sentence generating

--- project1.py
+++ project1.py
@@ -1,11 +1,12 @@
 
 # coding: utf-8
 
-# In[46]:
+# In[88]:
 
 from collections import defaultdict
 import operator
 import random
+import numpy as np
 
 def get_corpus(file):
     corpus = []
@@ -49,9 +50,11 @@ def get_bigram_freqs(corpus, unigram_freqs):
     return bi_count
 
 ### TO BE MODIFIED LATER
-def get_next_word_unigram(sentence, corpus_set):
-    return get_random_word(corpus_set)
-
+def get_next_word_unigram(corpus):
+    keys = [key for key in sorted(corpus, key=corpus.get)]
+    probs = [corpus[key] for key in sorted(corpus, key=corpus.get)]
+    return np.random.choice(keys, 1, probs)[0]
+    
 ### TO BE MODIFIED LATER
 def get_next_word_bigram(sentence, corpus):
     last_word = sentence[len(sentence) - 1]
@@ -60,60 +63,44 @@ def get_next_word_bigram(sentence, corpus):
     for key in corpus:
         if key[0] == last_word:
             candidates[key[1]] = corpus[key]
-            total_prob += corpus[key]
-    cumulative_prob = 0.0
-    for key in candidates:
-        term_prob = candidates[key] / total_prob
-        cumulative_prob += term_prob
-        candidates[key] = cumulative_prob
     
-    rand = random.uniform(0.0, 1.0)
-    prev = 0.0
-    for key in sorted(candidates, key=candidates.get):
-        if rand > prev and rand <= candidates[key]:
-            return key
-        else:
-            prev = candidates[key]
-
-    return max(candidates.items(), key=operator.itemgetter(1))[0]
+    keys = [key for key in sorted(candidates, key=candidates.get)]
+    probs = [candidates[key] for key in sorted(candidates, key=candidates.get)]
+    return np.random.choice(keys, 1, probs)[0]
 
 pos_file = 'SentimentDataset/Train/pos.txt'
 pos_corpus = get_corpus(pos_file)
 
 
-# In[47]:
+# In[89]:
 
 pos_unigram_freqs = get_unigram_freqs(pos_corpus)
 pos_unigram_freqs
 
 
-# In[48]:
+# In[90]:
 
 pos_bigram_freqs = get_bigram_freqs(pos_corpus, pos_unigram_freqs)
 pos_bigram_freqs
 
 
-# In[41]:
+# In[95]:
 
-get_next_word_bigram(['sum'], pos_bigram_freqs)
-
-
-# In[164]:
-
-start = get_random_word(pos_all_words)
-start
-for i in range(0, 20):
-    nxt = get_next_word_unigram(start, pos_unigrams)
+### Generate sentence using unigram model
+start = get_random_word(pos_corpus)
+for i in range(0, 10):
+    nxt = get_next_word_unigram(pos_unigram_freqs)
     start.append(nxt)
     if nxt == '.':
         break
     print(start)
 
 
-# In[57]:
+# In[96]:
 
+### Generate sentence using bigram model
 bi_start = get_random_word(pos_corpus)
-for i in range(0, 5):
+for i in range(0, 10):
     nxt = get_next_word_bigram(bi_start, pos_bigram_freqs)
     bi_start.append(nxt)
     if nxt == '.':

eb06a3efbb257a02f68e8446f91a71f3fcbdc41b
nec02349 <stephenhuang804@gmail.com>
2017-09-10 17:32:30 -0400

    Updated sentence gen and refractored code
    Updated sentence gen methods and deleted some unused methods

--- project1.py
+++ project1.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[88]:
+# In[115]:
 
 from collections import defaultdict
 import operator
@@ -12,17 +12,14 @@ def get_corpus(file):
     corpus = []
     f = open(file, 'r')
     for line in f:
-        new_line = []
+        # append start word
+        new_line = ['<s>']
         for word in line.split():
             new_line.append(word.lower())
+        # append stop word
+        new_line.append('</s>')
         corpus.append(new_line)
     return corpus
-
-def get_corpus_set(corpus):
-    return set([word for word_list in corpus for word in word_list])
-
-def get_random_word(corpus):
-    return random.sample(get_corpus_set(corpus), 1)
 
 # the frequency of each word in the corpus, eg P(word = A)
 def get_unigram_freqs(corpus):
@@ -49,13 +46,13 @@ def get_bigram_freqs(corpus, unigram_freqs):
         bi_count[key] = (bi_count[key] * 1.0 / total_count) / unigram_freqs[key[0]]
     return bi_count
 
-### TO BE MODIFIED LATER
+# generate the next random word using unigram model
 def get_next_word_unigram(corpus):
     keys = [key for key in sorted(corpus, key=corpus.get)]
     probs = [corpus[key] for key in sorted(corpus, key=corpus.get)]
     return np.random.choice(keys, 1, probs)[0]
     
-### TO BE MODIFIED LATER
+# generate the next random word using bigram model
 def get_next_word_bigram(sentence, corpus):
     last_word = sentence[len(sentence) - 1]
     candidates = defaultdict(float)
@@ -72,40 +69,47 @@ pos_file = 'SentimentDataset/Train/pos.txt'
 pos_corpus = get_corpus(pos_file)
 
 
-# In[89]:
+# In[116]:
 
 pos_unigram_freqs = get_unigram_freqs(pos_corpus)
-pos_unigram_freqs
+#pos_unigram_freqs
 
 
-# In[90]:
+# In[117]:
 
 pos_bigram_freqs = get_bigram_freqs(pos_corpus, pos_unigram_freqs)
-pos_bigram_freqs
+#pos_bigram_freqs
 
 
-# In[95]:
+# In[121]:
 
-### Generate sentence using unigram model
-start = get_random_word(pos_corpus)
-for i in range(0, 10):
-    nxt = get_next_word_unigram(pos_unigram_freqs)
-    start.append(nxt)
-    if nxt == '.':
-        break
-    print(start)
+# Generate a sentence using unigram model
+def generate_unigram_sentence(length):
+    start = ['<s>']
+    for i in range(0, length):
+        nxt = get_next_word_unigram(pos_unigram_freqs)
+        start.append(nxt)
+        if nxt == '</s>':
+            break
+        
+    print(' '.join(start[1:]))
+
+generate_unigram_sentence(20)
 
 
-# In[96]:
+# In[124]:
 
-### Generate sentence using bigram model
-bi_start = get_random_word(pos_corpus)
-for i in range(0, 10):
-    nxt = get_next_word_bigram(bi_start, pos_bigram_freqs)
-    bi_start.append(nxt)
-    if nxt == '.':
-        break
-    print(bi_start)
+# Generate a sentence using bigram model
+def generate_bigram_sentence(length):
+    bi_start = ['<s>']
+    for i in range(0, length):
+        nxt = get_next_word_bigram(bi_start, pos_bigram_freqs)
+        bi_start.append(nxt)
+        if nxt == '</s>':
+            break
+    print(' '.join(bi_start[1:]))
+    
+generate_bigram_sentence(20)
 
 
 # In[ ]:
